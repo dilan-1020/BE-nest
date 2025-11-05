658e72aa64994e33f8a01cdf28abd82ec6911bdc
딜런 <dilan@netstream.co.kr>
2025-11-05 14:44:57 +0900

    feat : swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import {SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,17 @@ async function bootstrap() {
     Credential:true,
     exposedHeaders : ['Authorization'],
   });
-  await app.listen(process.env.PORT ?? 3001
+
+  const config = new DocumentBuilder()
+    .setTitle("Nest BE")
+    .setDescription("Nest BE")
+    .setVersion('1.0')
+    .addTag('BE')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
+  await app.listen(configService.get("LOCAL_PORT") ?? 3007
   );
   logger.log(`Server is running`);
 }
